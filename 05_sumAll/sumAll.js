60fc7d27902d268a3df61923fb9603eddfb34703
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2022-06-13 00:10:53 +0400

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (num1 < num2) {
+        start = num1;
+        end = num2;
+    }
+    else {
+        start = num2;
+        end = num1;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
