3f49638f3bbe99876c11bc8935002d9ac7181407
Bhakti <ghaghdabhakti@gmail.com>
2022-06-12 12:46:13 +0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let num = number; //copying number parameter to temporary variable
+    let returnString = '';
 
+    if (num < 0) return 'ERROR';
+    
+    while (num--)
+    {
+        returnString += string;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
