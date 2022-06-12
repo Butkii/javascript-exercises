4d33908ee0a30ffb4fed167bc73716fd9e4417a3
Bhakti <ghaghdabhakti@gmail.com>
2022-06-12 12:46:29 +0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let n = str.length;
+    let temp = '';
 
+    for (let i = n-1; i >= 0; i--)
+    {
+        temp += str[i];
+    }
+
+    return temp;
 };
 
 // Do not edit below this line
