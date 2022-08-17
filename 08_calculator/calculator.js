f8a2e83b3be4b60722fde44db5a342806c655d70
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2022-08-17 13:47:08 +0400

    Completed exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, nextItem) => total + nextItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, nextItem) => total * nextItem, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	const arr = Array.from(Array(x), (_, i) => i+1);
+  return arr.reduce((total, nextItem) => total * nextItem, 1);
 };
 
 // Do not edit below this line
